6a96ca2439d4530f41359e93548276539c5914e0
manyowastephano <manyowastephano@gmail.com>
2025-09-21 22:52:58 +0200

    env variable conf

--- src/config.js
+++ src/config.js
@@ -1,2 +1,5 @@
-export const BASE_URL = 'http://localhost:8000/';
+//export const BASE_URL = 'http://localhost:8000/';
 
+// src/config/index.js
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000';
+//export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `${BASE_URL}/api`;
--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,8 @@
 // src/config/index.js
-export const BASE_URL = 'http://localhost:8000';
-export const API_BASE_URL = `${BASE_URL}/api`;
+//export const BASE_URL = 'http://localhost:8000';
+//export const API_BASE_URL = `${BASE_URL}/api`;
 
+
+// src/config/index.js
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `${BASE_URL}/api`;

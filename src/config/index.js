a5f033f56366b6bf8c9dad7b707ba0a49b0c4cea
manyowastephano <manyowastephano@gmail.com>
2025-09-22 16:09:06 +0200

    cors

--- src/config/index.js
+++ src/config/index.js
@@ -6,3 +6,6 @@
 // src/config/index.js
 export const BASE_URL = process.env.REACT_APP_BASE_URL || 'https://mubassomase-1.onrender.com';
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || `${BASE_URL}/api`;
+
+console.log('API_BASE_URL:', API_BASE_URL);
+console.log('BASE_URL:', BASE_URL);
